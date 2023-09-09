84df3844290bb956b51334687ee5b3afb6e41ca3
Dimas Yudha Pratama <pratama@dimasyudha.tech>
2023-09-09 10:13:18 +0700

    feat: enhance Dockerfile.next with Node.js 18.17.1 and npm 9.6.6
    - Dockerfile.next is now updated to include Node.js version 18.17.1 and npm version 9.6.6.
    
    Signed-off-by: Dimas Yudha Pratama <pratama@dimasyudha.tech>

--- Dockerfile.go
+++ Dockerfile.go
@@ -12,7 +12,7 @@ RUN echo -e "https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpin
              https://dl-cdn.alpinelinux.org/alpine/edge/testing/\n \
              https://dl-cdn.alpinelinux.org/alpine/edge/community/\n \
              https://dl-cdn.alpinelinux.org/alpine/edge/main/" > /etc/apk/repositories && \
-   sed -i 's/^[ \t]*//;s/[ \t]*$//' /etc/apk/repositories
+    sed -i 's/^[ \t]*//;s/[ \t]*$//' /etc/apk/repositories
 
 RUN apk update && \
     apk add --no-cache tini=0.19.0-r1 \
