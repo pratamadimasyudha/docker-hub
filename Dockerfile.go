662a5b25243404163a3cbada7e0f4eda20ba30c0
Dimas Yudha Pratama <pratama@dimasyudha.tech>
2023-09-09 10:10:13 +0700

    feat: add Dockerfile for Go 1.20.7 and PNPM v7.15.0 environments
    - Introduce Dockerfile.go for Alpine 3.18.3 based Go 1.20.7 setup.
    - Add Dockerfile.next for Alpine 3.18.3 with pnpm v7.15.0 installation.
    
    Signed-off-by: Dimas Yudha Pratama <pratama@dimasyudha.tech>

---
+++ Dockerfile.go
@@ -0,0 +1,20 @@
+FROM alpine:3.18.3
+
+RUN addgroup -g 10001 \
+             -S nonroot && \
+    adduser  -u 10000 \
+             -G nonroot \
+             -h /home/nonroot \
+             -S nonroot
+
+RUN echo -e "https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/main/\n \
+             https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community/\n \
+             https://dl-cdn.alpinelinux.org/alpine/edge/testing/\n \
+             https://dl-cdn.alpinelinux.org/alpine/edge/community/\n \
+             https://dl-cdn.alpinelinux.org/alpine/edge/main/" > /etc/apk/repositories && \
+   sed -i 's/^[ \t]*//;s/[ \t]*$//' /etc/apk/repositories
+
+RUN apk update && \
+    apk add --no-cache tini=0.19.0-r1 \
+                       bind-tools=9.18.16-r0 \
+                       go=1.20.7-r0
